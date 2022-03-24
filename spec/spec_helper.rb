8ed729173cb058bb090b79e25b8079ade0d3a242
Frederick Cheung <frederick.cheung@gmail.com>
2022-03-24 13:15:12 +0000

    force gc after suite has finished

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,5 +6,7 @@ $: << File.dirname(__FILE__) + '/../lib'
 require 'corefoundation'
 
 RSpec.configure do |config|
-  
+  config.after(:suite) do
+    GC.start #flush out any issues with finalizers
+  end
 end
